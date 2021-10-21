d20dbd994475598648dc1e3068a3bad8b0208cd3
Wojciech Bubolka <wojciech@websquare.co.uk>
2021-10-21 10:32:07 +0100

    add print_result()

--- library/includes/fun/fun-helper.php
+++ library/includes/fun/fun-helper.php
@@ -1,3 +1,22 @@
 <?php
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
+
+if (!function_exists('print_result')) {
+	function print_result($result = "", $admin = false)
+	{
+		if ($result != "") {
+			if ($admin) {
+				if (current_user_can('manage_options')) {
+					echo '<pre>';
+					print_r($result);
+					echo '</pre>';
+				}
+			} else {
+				echo '<pre>';
+				print_r($result);
+				echo '</pre>';
+			}
+		}
+	}
+}
